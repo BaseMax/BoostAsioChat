43f4aca035340fc9075db1ec60caa2dbc87b9b0c
Max Base <MaxBaseCode@Gmail.Com>
2019-06-08 19:08:02 +0430

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -3,62 +3,63 @@
 #include <cstdlib>
 #include <deque>
 #include <boost/asio.hpp>
-#include "chat_message.hpp"
+#include "message.hpp"
 using boost::asio::ip::tcp;
-typedef std::deque<chat_message> chat_message_queue;
-class chat_client {
+using namespace std;
+typedef deque<message> messageQueue;
+class client {
     public:
-        chat_client(boost::asio::io_context& io_context, const tcp::resolver::results_type& endpoints) : io_context_(io_context), socket_(io_context) {
+        client(boost::asio::io_context& context, const tcp::resolver::results_type& endpoints) : context_(context), socket_(context) {
             do_connect(endpoints);
         }
-        void write(const chat_message& msg) {
-            boost::asio::post(io_context_, [this, msg]() {
+        void write(const message& msg) {
+            boost::asio::post(context_, [this, msg]() {
                 bool write_in_progress = !write_msgs_.empty();
                 write_msgs_.push_back(msg);
                 if(!write_in_progress) {
-                    do_write();
+                    write();
                 }
             });
         }
         void close() {
-            boost::asio::post(io_context_, [this]() { socket_.close(); });
+            boost::asio::post(context_, [this]() { socket_.close(); });
         }
     private:
         void do_connect(const tcp::resolver::results_type& endpoints) {
             boost::asio::async_connect(socket_, endpoints, [this](boost::system::error_code ec, tcp::endpoint) {
                 if(!ec) {
-                    do_read_header();
+                    readHeader();
                 }
             });
         }
-        void do_read_header() {
-            boost::asio::async_read(socket_, boost::asio::buffer(read_msg_.data(), chat_message::header_length), [this](boost::system::error_code ec, std::size_t) {
-                if(!ec && read_msg_.decode_header()) {
-                    do_read_body();
+        void readHeader() {
+            boost::asio::async_read(socket_, boost::asio::buffer(read_msg_.data(), message::header_length), [this](boost::system::error_code ec, size_t) {
+                if(!ec && read_msg_.decodeHeader()) {
+                    readBody();
                 }
                 else {
                     socket_.close();
                 }
             });
         }
-        void do_read_body() {
-            boost::asio::async_read(socket_, boost::asio::buffer(read_msg_.body(), read_msg_.body_length()), [this](boost::system::error_code ec, std::size_t) {
+        void readBody() {
+            boost::asio::async_read(socket_, boost::asio::buffer(read_msg_.body(), read_msg_.bodyLength()), [this](boost::system::error_code ec, size_t) {
                 if(!ec) {
-                    std::cout.write(read_msg_.body(), read_msg_.body_length());
-                    std::cout << "\n";
-                    do_read_header();
+                    cout.write(read_msg_.body(), read_msg_.bodyLength());
+                    cout << "\n";
+                    readHeader();
                 }
                 else {
                     socket_.close();
                 }
             });
         }
-        void do_write() {
-            boost::asio::async_write(socket_, boost::asio::buffer(write_msgs_.front().data(), write_msgs_.front().length()), [this](boost::system::error_code ec, std::size_t) {
+        void write() {
+            boost::asio::async_write(socket_, boost::asio::buffer(write_msgs_.front().data(), write_msgs_.front().length()), [this](boost::system::error_code ec, size_t) {
                 if(!ec) {
                     write_msgs_.pop_front();
                     if(!write_msgs_.empty()) {
-                        do_write();
+                        write();
                     }
                 }
                 else {
@@ -66,35 +67,35 @@ class chat_client {
                 }
             });
         }
-        boost::asio::io_context& io_context_;
+        boost::asio::io_context& context_;
         tcp::socket socket_;
-        chat_message read_msg_;
-        chat_message_queue write_msgs_;
+        message read_msg_;
+        messageQueue write_msgs_;
 };
 int main(int argc, char* argv[]) {
     try {
         if(argc != 3) {
-            std::cerr << "Usage: chat_client <host> <port>\n";
+            cerr << "Usage: client <host> <port>\n";
             return 1;
         }
-        boost::asio::io_context io_context;
-        tcp::resolver resolver(io_context);
+        boost::asio::io_context context;
+        tcp::resolver resolver(context);
         auto endpoints = resolver.resolve(argv[1], argv[2]);
-        chat_client c(io_context, endpoints);
-        std::thread t([&io_context](){ io_context.run(); });
-        char line[chat_message::max_body_length + 1];
-        while(std::cin.getline(line, chat_message::max_body_length + 1)) {
-            chat_message msg;
-            msg.body_length(std::strlen(line));
-            std::memcpy(msg.body(), line, msg.body_length());
-            msg.encode_header();
+        client c(context, endpoints);
+        thread t([&context](){ context.run(); });
+        char line[message::max_bodyLength + 1];
+        while(cin.getline(line, message::max_bodyLength + 1)) {
+            message msg;
+            msg.bodyLength(strlen(line));
+            memcpy(msg.body(), line, msg.bodyLength());
+            msg.encodeHeader();
             c.write(msg);
         }
         c.close();
         t.join();
     }
-    catch (std::exception& e) {
-        std::cerr << "Exception: " << e.what() << "\n";
+    catch (exception& e) {
+        cerr << "Exception: " << e.what() << "\n";
     }
     return 0;
 }
